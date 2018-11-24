e3cd0a5a47e4781ac207f36d141a70425bf8d4ff
azat.serzhanov <azat.serzhanov@btsdigital.kz>
2018-11-24 19:48:38 +0600

    [FIXED] refactored

--- app/src/main/java/kz/tilsimsozder/activity/main/MainActivity.kt
+++ app/src/main/java/kz/tilsimsozder/activity/main/MainActivity.kt
@@ -7,7 +7,6 @@ import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.DisplayMetrics
-import android.util.Log
 import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
@@ -49,7 +48,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setupStartNotification()
         setupService()
         fontSizeSeekBarSetup()
-        firstStartSetup()
+        pushNotificationNumber()
     }
 
     override fun onBackPressed() {
@@ -112,7 +111,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return true
     }
 
-    private fun firstStartSetup() {
+    private fun pushNotificationNumber() {
         if (MyService.RANDOM_TILSIM != 0) {
             TextViewHeader.text = LIST_TITLE_DATA_TILSIM[MyService.RANDOM_TILSIM]
             TextViewContent.text = LIST_CONTENT_DATA_TILSIM[MyService.RANDOM_TILSIM]
@@ -280,7 +279,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         if (START_NOTIFICATION) {
             Alerter.create(this)
                 .setTitle("Бұл жолығы жаңартылымдар")
-                .setText("Үш сағат сайын сізге кездейсоқ тылсым сөз келіп отырады")
+                .setText("Дұғалар мен мәзір қосылды")
                 .setBackgroundColorRes(R.color.colorNotification)
                 .show()
         }
