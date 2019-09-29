f3e972adbd00ecc401d0bf94a4032356ddc74eda
azatserzhan <azatserzhan@gmail.com>
2019-09-29 11:15:24 +0600

    [ADDED] new navigation menu

--- app/src/main/java/kz/tilsimsozder/TilsimSozderActivity.kt
+++ app/src/main/java/kz/tilsimsozder/TilsimSozderActivity.kt
@@ -1,15 +1,13 @@
 package kz.tilsimsozder
 
-import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.Gravity
-import android.view.View
 import androidx.core.view.GravityCompat
-import kotlinx.android.synthetic.main.tilsim_sozder_activity.nav_view
-import kotlinx.android.synthetic.main.tilsim_sozder_activity.tilsimDrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import kotlinx.android.synthetic.main.tilsim_sozder_activity.*
 import kz.tilsimsozder.common.BaseActivity
 import kz.tilsimsozder.bots.ui.BotFragment
 import kz.tilsimsozder.firebase.Analytics
@@ -19,6 +17,12 @@ import kz.tilsimsozder.preference.FragmentName
 import kz.tilsimsozder.preference.SharedPreference
 import kz.tilsimsozder.service.TilsimService
 import kz.tilsimsozder.tilsim.ui.TilsimFragment
+import androidx.viewpager.widget.ViewPager
+
+private const val PRAYER_PAGE_ID = 0
+private const val TILSIM_PAGE_ID = 1
+private const val NEWS_PAGE_ID = 2
+private const val SERVICE_PAGE_ID = 3
 
 class TilsimSozderActivity : BaseActivity() {
 
@@ -35,6 +39,7 @@ class TilsimSozderActivity : BaseActivity() {
         isThemeDark = SharedPreference(this).getIsThemeDark()
         setupStyle()
         setupNavMenu()
+        setupViewPager()
 
         when (SharedPreference(baseContext).getCurrentFragmentName()) {
             FragmentName.PRAYER.ordinal -> replaceFragment(PrayersFragment.create())
@@ -43,13 +48,6 @@ class TilsimSozderActivity : BaseActivity() {
             FragmentName.NEWS.ordinal -> replaceFragment(NewsFragment.create())
             else -> replaceFragment(TilsimFragment.create())
         }
-    }
-
-    override fun onCreateView(name: String?, context: Context?, attrs: AttributeSet?): View? {
-        return super.onCreateView(name, context, attrs)
-
-        // setSupportActionBar(toolbar)
-        // tilsimDrawerLayout.openDrawer(Gravity.LEFT)
     }
 
     override fun onBackPressed() {
@@ -128,5 +126,76 @@ class TilsimSozderActivity : BaseActivity() {
             true
 
         }
+    }
+
+    private fun setupViewPager() {
+        if (viewPager != null) {
+            val adapter = ViewPagerAdapter(supportFragmentManager)
+            viewPager.adapter = adapter
+
+            viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+                override fun onPageScrollStateChanged(state: Int) {}
+
+                override fun onPageSelected(position: Int) {
+                    setupIconMenu(position)
+                }
+
+                override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+            })
+
+            menuItemPrayer.setOnClickListener {
+                viewPager.setCurrentItem(PRAYER_PAGE_ID, true)
+                setupIconMenu(PRAYER_PAGE_ID)
+            }
+            menuItemTilsim.setOnClickListener {
+                viewPager.setCurrentItem(TILSIM_PAGE_ID, true)
+                setupIconMenu(TILSIM_PAGE_ID)
+            }
+            menuItemNews.setOnClickListener {
+                viewPager.setCurrentItem(NEWS_PAGE_ID, true)
+                setupIconMenu(NEWS_PAGE_ID)
+            }
+            menuItemService.setOnClickListener {
+                viewPager.setCurrentItem(SERVICE_PAGE_ID, true)
+                setupIconMenu(SERVICE_PAGE_ID)
+            }
+        }
+    }
+
+    private fun setupIconMenu(position: Int) {
+        menuItemPrayer.background = getDrawable(R.drawable.ic_prayer)
+        menuItemTilsim.background = getDrawable(R.drawable.ic_tilsim)
+        menuItemNews.background = getDrawable(R.drawable.ic_news)
+        menuItemService.background = getDrawable(R.drawable.ic_service)
+
+        when (position) {
+            PRAYER_PAGE_ID -> menuItemPrayer.background = getDrawable(R.drawable.ic_prayer_active)
+            TILSIM_PAGE_ID -> menuItemTilsim.background = getDrawable(R.drawable.ic_tilsim_active)
+            NEWS_PAGE_ID -> menuItemNews.background = getDrawable(R.drawable.ic_news_active)
+            SERVICE_PAGE_ID -> menuItemService.background = getDrawable(R.drawable.ic_service_active)
+        }
+
+    }
+}
+
+internal class ViewPagerAdapter(
+        fragmentManager: FragmentManager
+) : FragmentPagerAdapter(fragmentManager) {
+    private var count = 4
+
+    override fun getItem(position: Int): Fragment? {
+        var fragment: Fragment? = null
+        when (position) {
+            PRAYER_PAGE_ID -> fragment = PrayersFragment.create()
+            TILSIM_PAGE_ID -> fragment = TilsimFragment.create()
+            NEWS_PAGE_ID -> fragment = NewsFragment.create()
+            SERVICE_PAGE_ID -> fragment = BotFragment.create()
+        }
+
+        return fragment
+    }
+
+    override fun getCount(): Int {
+        return count
     }
 }
