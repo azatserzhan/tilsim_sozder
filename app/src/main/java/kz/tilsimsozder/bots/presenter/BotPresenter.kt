194c93a9f40b38f22b16c29aa0eb3a36a7b539cb
azatserzhan <azatserzhan@gmail.com>
2019-10-12 18:02:52 +0600

    [FIXED] bots and news uz

--- app/src/main/java/kz/tilsimsozder/bots/presenter/BotPresenter.kt
+++ app/src/main/java/kz/tilsimsozder/bots/presenter/BotPresenter.kt
@@ -6,29 +6,47 @@ import kz.tilsimsozder.bots.contract.BotContract
 import kz.tilsimsozder.bots.model.Bot
 import kz.tilsimsozder.common.BasePresenter
 import kz.tilsimsozder.firebase.Analytics
+import kz.tilsimsozder.preference.SharedPreference
+import kz.tilsimsozder.preference.SupportLanguage
 
 class BotPresenter(private val analytics: Analytics, val context: Context) : BasePresenter<BotContract.View>(),
-        BotContract.Presenter {
+    BotContract.Presenter {
 
     override fun loadBots() {
-        val bots = listOf(
-                Bot(
+        when (SharedPreference(context).getLanguageCode()) {
+            SupportLanguage.KZ.code -> {
+                val bots = listOf(
+                    Bot(
                         "Бот: Астана",
                         "Астана қаласы бойынша Дұғалық кездесу немесе басқа да проекттердің статистикасын жүргізетін сервис",
                         "https://t.me/Astana_bahai_bot",
                         R.drawable.astana_bot
-                ),
-                Bot("Бот: Алматы",
+                    ),
+                    Bot("Бот: Алматы",
                         "Алматы қаласы бойынша Дұғалық кездесу немесе басқа да проекттердің статистикасын жүргізетін сервис",
                         "https://t.me/bahai_almaty_bot", R.drawable.almaty_bot),
-                Bot("Telegram: Медиа Канал Бахаи",
+                    Bot("Telegram: Медиа Канал Бахаи",
                         "",
                         "https://t.me/mediabahai", R.drawable.bot),
-                Bot("Telegram: Цитаты Бахаи",
+                    Bot("Telegram: Цитаты Бахаи",
                         "",
                         "https://t.me/bahaiwisdom", R.drawable.bot)
-        )
+                )
 
-        view?.showBots(bots)
+                view?.showBots(bots)
+            }
+            SupportLanguage.UZ.code -> {
+                val bots = listOf(
+                    Bot("Telegram: Медиа Канал Бахаи",
+                        "",
+                        "https://t.me/mediabahai", R.drawable.bot),
+                    Bot("Telegram: Цитаты Бахаи",
+                        "",
+                        "https://t.me/bahaiwisdom", R.drawable.bot)
+                )
+
+                view?.showBots(bots)
+            }
+        }
     }
 }
--- app/src/main/java/kz/tilsimsozder/news/presenter/NewsPresenter.kt
+++ app/src/main/java/kz/tilsimsozder/news/presenter/NewsPresenter.kt
@@ -6,39 +6,68 @@ import kz.tilsimsozder.bots.model.Bot
 import kz.tilsimsozder.common.BasePresenter
 import kz.tilsimsozder.firebase.Analytics
 import kz.tilsimsozder.news.contract.NewsContract
+import kz.tilsimsozder.preference.SharedPreference
+import kz.tilsimsozder.preference.SupportLanguage
 
-private const val URL_NEWS = "http://bahai.kz/?page_id=19&lang=kk"
 
 class NewsPresenter(private val analytics: Analytics, val context: Context) : BasePresenter<NewsContract.View>(),
     NewsContract.Presenter {
 
     override fun loadNews() {
-        val bots = listOf(
-            Bot(
-                "www.bahai.kz",
-                "Қазақстан бахаиларының ресими сайтынан жаңалықтар",
-                "http://www.bahai.kz",
-                R.drawable.ic_bahai_kz
-            ),
-            Bot("www.bahai.org",
-                "Бүкл әлемдегі бахаиларының ресми сайты",
-                "https:www.bahai.org",
-                R.drawable.ic_bahai_org
-            ),
-            Bot("Восхождение света",
-                "Баб туралы бейне фильм.\n" +
-                    "Орыс тілінде",
-                "https://bicentenary.bahai.org/ru/the-bab/",
-                R.drawable.ic_bab
-            ),
-            Bot("YouTube: Бахаи Казахстана",
-                "",
-                "https://www.youtube.com/channel/UCSOVNuKVx_HovSbTpRZnt3Q", R.drawable.youtube_bahai_channel),
-            Bot("Instagram: kazakhstan_bahai",
-                "",
-                "https://www.instagram.com/kazakhstan_bahai/", R.drawable.insta_bahai_channel)
-        )
+        when(SharedPreference(context).getLanguageCode()) {
+            SupportLanguage.KZ.code -> {
+                val bots = listOf(
+                    Bot(
+                        "www.bahai.kz",
+                        "Қазақстан бахаиларының ресими сайтынан жаңалықтар",
+                        "http://www.bahai.kz",
+                        R.drawable.ic_bahai_kz
+                    ),
+                    Bot("https://www.bahai.org/ru/",
+                        "Бүкл әлемдегі бахаиларының ресми сайты",
+                        "https:https://www.bahai.org/ru/",
+                        R.drawable.ic_bahai_org
+                    ),
+                    Bot("Восхождение света",
+                        "Баб туралы бейне фильм.\n" +
+                            "Орыс тілінде",
+                        "https://bicentenary.bahai.org/ru/the-bab/",
+                        R.drawable.ic_bab
+                    ),
+                    Bot("YouTube: Бахаи Казахстана",
+                        "",
+                        "https://www.youtube.com/channel/UCSOVNuKVx_HovSbTpRZnt3Q", R.drawable.youtube_bahai_channel),
+                    Bot("Instagram: kazakhstan_bahai",
+                        "",
+                        "https://www.instagram.com/kazakhstan_bahai/", R.drawable.insta_bahai_channel)
+                )
 
-        view?.showNews(bots)
+                view?.showNews(bots)
+            }
+            SupportLanguage.UZ.code -> {
+                val bots = listOf(
+                    Bot(
+                        "https://baxoi.uz/uzl/",
+                        "O'zbekiston Bahoiy jamoasining rasmiy sayti",
+                        "http://www.bahai.kz",
+                        R.drawable.ic_bahai_kz
+                    ),
+                    Bot("https://www.bahai.org/ru/",
+                        "Вебсайт всемирной общины бахаи",
+                        "https:https://www.bahai.org/ru/",
+                        R.drawable.ic_bahai_org
+                    ),
+                    Bot("Восхождение света",
+                        "СПЕЦИАЛЬНО ПОДГОТОВЛЕННЫЙ ФИЛЬМ\n" +
+                            "подготовленный к двухсотлетию",
+                        "https://bicentenary.bahai.org/ru/the-bab/",
+                        R.drawable.ic_bab
+                    )
+                )
+
+                view?.showNews(bots)
+            }
+        }
+
     }
 }
